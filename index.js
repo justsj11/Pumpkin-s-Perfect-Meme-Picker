59526f44d2c63558a7bcfae790705d86238dd08a
Sagnik Jana <justsj11@gmail.com>
2022-10-23 00:51:04 +0530

    Final updates to index.js

--- index.js
+++ index.js
@@ -3,10 +3,19 @@ import { catsData } from '/data.js'
 const emotionRadios = document.getElementById('emotion-radios')
 const getImageBtn = document.getElementById('get-image-btn')
 const gifsOnlyOption = document.getElementById('gifs-only-option')
+const memeModalInner = document.getElementById('meme-modal-inner')
+const memeModal = document.getElementById('meme-modal')
+const memeModalCloseBtn = document.getElementById('meme-modal-close-btn')
 
 emotionRadios.addEventListener('change', highlightCheckedOption)
 
-getImageBtn.addEventListener('click', getMatchingCatsArray)
+getImageBtn.addEventListener('click', renderCat)
+
+memeModalCloseBtn.addEventListener('click', closeModal)
+
+function closeModal(){
+    memeModal.style.display = 'none'
+}
 
 function highlightCheckedOption(e){
     const radios = document.getElementsByClassName('radio')
@@ -22,17 +31,40 @@ function getMatchingCatsArray(){
         const isGif = gifsOnlyOption.checked
         
         const matchingCatsArray = catsData.filter(function(cat){
-/*
-Challenge:
-1. Change the .filter() method's function so it returns an 
-   array that only has GIFs if the 'GIFs only' option is 
-   checked. If the 'GIFs only' option is not checked, it
-   should return an array of all matches as it does now.
-*/ 
-            return cat.emotionTags.includes(selectedEmotion)
+            
+            if(isGif){
+                return cat.emotionTags.includes(selectedEmotion) && cat.isGif
+            }
+            else{
+                return cat.emotionTags.includes(selectedEmotion)
+            }            
         })
-        console.log(matchingCatsArray)
+        return matchingCatsArray 
     }  
+}
+
+function getSingleCatObject(){
+    const catsArray = getMatchingCatsArray()
+    
+    if(catsArray.length === 1){
+        return catsArray[0]
+    }
+    else{
+        const randomNumber = Math.floor(Math.random() * catsArray.length)
+        return catsArray[randomNumber]
+    }
+}
+
+function renderCat(){
+    const catObject = getSingleCatObject()
+    memeModalInner.innerHTML =  `
+        <img 
+        class="cat-img" 
+        src="./images/${catObject.image}"
+        alt="${catObject.alt}"
+        >
+        `
+    memeModal.style.display = 'flex'
 }
 
 function getEmotionsArray(cats){
@@ -46,7 +78,6 @@ function getEmotionsArray(cats){
     }
     return emotionsArray
 }
-
 
 function renderEmotionsRadios(cats){
         
